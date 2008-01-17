1bb63d75e70a13f60f7a5b01aa59f204bd31213e
Fiete <41542859+FieteFietz@users.noreply.github.com>
2008-01-17 16:55:37 +0000

    initial 2

---
+++ MapCleaner/src/magellan/plugin/mapcleaner/DelNamedOceans.java
@@ -0,0 +1,43 @@
+package magellan.plugin.mapcleaner;
+
+import java.util.Iterator;
+
+import magellan.plugin.mapcleaner.MsgBox;
+
+import magellan.client.Client;
+import magellan.library.GameData;
+import magellan.library.Region;
+import magellan.library.event.GameDataEvent;
+import magellan.library.utils.Regions;
+
+public class DelNamedOceans implements MapCleanAction{
+
+	/**
+	 * The Name to display in MenuList
+	 * @return
+	 */
+	public String getName(){
+		return "Delete named oceans";
+	}
+	
+	/**
+	 * Action to be called if MenuEntry is selected
+	 * @param client
+	 */
+	public void clean(Client client){
+		GameData data = client.getData();
+		int counter=0;
+		if (data!=null && data.regions()!=null && data.rules!=null){
+			for (Iterator<Region> iter = data.regions().values().iterator();iter.hasNext();){
+				Region actRegion = (Region)iter.next();
+				if (actRegion!=null && actRegion.getRegionType()!=null && Regions.getOceanRegionTypes(data.rules).get(actRegion.getRegionType().getID())!=null && actRegion.getName()!=null){
+					actRegion.setName(null);
+					counter++;
+				}
+			}
+			client.getDispatcher().fire(new GameDataEvent(this, data, true));
+		}
+		new MsgBox(client,"Done with " + counter + " regions.","OK",false);
+	}
+	
+}
