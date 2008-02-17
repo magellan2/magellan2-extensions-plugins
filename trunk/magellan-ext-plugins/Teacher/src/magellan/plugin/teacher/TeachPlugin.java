0e44080c727e213f93cac92572e50e484084d0fa
stm_ <stm_@1c5abfc5-b992-4a1f-8ccd-9ce068e22774>
2008-02-17 14:34:23 +0000

    cleaned version

--- trunk/magellan-ext-plugins/Teacher/src/magellan/plugin/teacher/TeachPlugin.java
+++ trunk/magellan-ext-plugins/Teacher/src/magellan/plugin/teacher/TeachPlugin.java
@@ -1,10 +1,4 @@
-// class magellan.plugin.FFplugin.FFplugin
-// created on 04.07.2007
-//
 // Copyright 2003-2007 by magellan project team
-//
-// Author : $Fiete: $
-// $Id: $
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -43,108 +37,144 @@ import magellan.library.UnitContainer;
 import magellan.library.event.GameDataEvent;
 import magellan.library.utils.logging.Logger;
 
-
+/**
+ * This plug in facilitates making teaching orders. To be included in the process, a unit must have
+ * a meta order of the following type:
+ * 
+ * <pre>
+ * // $$L Talent1 value1 Talent2 value2
+ * </pre>
+ * 
+ * denotes a student learning two skills of different values
+ * 
+ * <pre>
+ * // $$L ALLES maxVal minVal
+ * </pre>
+ * 
+ * means that the student will learn any talent it already knows. The highest skill will have
+ * maxVal; skills with lower ranks will have values down to minVal
+ * 
+ * <pre>
+ * // $$T Talent1 maxDiff1 Talent2 maxDiff2
+ * </pre>
+ * 
+ * denotes a teacher teaching two skills. Students having a skill differing more than maxDiff from
+ * the teachers talent are penalized. maxDiff==0 has the special meaning that there is no such
+ * penalty. maxDiff==1 means that the teacher will not teach this talent
+ * 
+ * <pre>
+ * // $$T ALLES maxDiff
+ * </pre>
+ * 
+ * denotes a teacher teaching all the skills he knows.
+ * 
+ * <pre>
+ * // $$T ALLES 0 Hiebwaffen 2
+ * </pre>
+ * 
+ * would also be feasible
+ * 
+ * <pre>
+ * // $namespace1$T ...
+ * </pre>
+ * <pre>
+ * // $namespace1$L ...
+ * </pre>
+ * 
+ * defines an order belonging to a namespace; it can be used to teaching only to units with certain
+ * namespace
+ * 
+ * It is feasible (in fact, desirable) for a unit to be teacher and student at the same time.
+ * 
+ *  @author stm
+ */
 public class TeachPlugin implements MagellanPlugIn, UnitContainerContextMenuProvider {
-  private static Logger log = null;
+	private static Logger log = null;
 
-  private Client client = null;
-  private Properties properties = null;
-  private GameData gd = null;
+	private Client client = null;
+	private Properties properties = null;
+	private GameData gd = null;
 
-  /**
-   * @see magellan.client.extern.MagellanPlugIn#init(magellan.client.Client,
-   *      java.util.Properties)
-   */
-  public void init(Client _client, Properties _properties) {
-    // init the plugin
-    this.client = _client;
-    this.properties = _properties;
+	/**
+	 * @see magellan.client.extern.MagellanPlugIn#init(magellan.client.Client, java.util.Properties)
+	 */
+	public void init(Client _client, Properties _properties) {
+		// init the plugin
+		this.client = _client;
+		this.properties = _properties;
 
-    log = Logger.getInstance(TeachPlugin.class);
-    log.info(getName() + " initialized...");
+		log = Logger.getInstance(TeachPlugin.class);
+		log.info(getName() + " initialized...");
 
-  }
+	}
 
-  /**
-   * @see magellan.client.extern.MagellanPlugIn#init(magellan.library.GameData)
-   */
-  public void init(GameData data) {
-    // init the report
-    this.gd = data;
-  }
+	/**
+	 * @see magellan.client.extern.MagellanPlugIn#init(magellan.library.GameData)
+	 */
+	public void init(GameData data) {
+		// init the report
+		this.gd = data;
+	}
 
-  /**
-   * @see magellan.client.extern.MagellanPlugIn#getMenuItems()
-   */
-  public List<JMenuItem> getMenuItems() {
-      return Collections.emptyList();
-  }
+	/**
+	 * @see magellan.client.extern.MagellanPlugIn#getMenuItems()
+	 */
+	public List<JMenuItem> getMenuItems() {
+		return Collections.emptyList();
+	}
 
-  /**
-   * @see magellan.client.extern.MagellanPlugIn#getName()
-   */
-  public String getName() {
-    return "FFplugin";
-  }
+	/**
+	 * @see magellan.client.extern.MagellanPlugIn#getName()
+	 */
+	public String getName() {
+		return "FFplugin";
+	}
 
-  /**
-   * @see magellan.client.swing.context.UnitContainerContextMenuProvider#createContextMenu(magellan.client.event.EventDispatcher,
-   *      magellan.library.GameData, magellan.library.UnitContainer)
-   */
-  public JMenuItem createContextMenu(final EventDispatcher dispatcher, final GameData data,
-      final UnitContainer container) {
-    JMenu menu = new JMenu("TeachTest");
+	/**
+	 * @see magellan.client.swing.context.UnitContainerContextMenuProvider#createContextMenu(magellan.client.event.EventDispatcher,
+	 *      magellan.library.GameData, magellan.library.UnitContainer)
+	 */
+	public JMenuItem createContextMenu(final EventDispatcher dispatcher, final GameData data,
+			final UnitContainer container) {
+		JMenu menu = new JMenu("TeachTest");
 
-    JMenuItem editMenu = new JMenuItem("teach");
-    editMenu.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        new Thread(new Runnable() {
+		// do teaching for this unit container
+		JMenuItem editMenu = new JMenuItem("teach");
+		editMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new Thread(new Runnable() {
 
-          public void run() {
-            Teacher.foo(container, new ProgressBarUI(client));
+					public void run() {
+						Teacher.teach(container, new ProgressBarUI(client));
 
-            dispatcher.fire(new GameDataEvent(client, client.getData()));
-          }
-        }).start();
-      }
-    });
-    menu.add(editMenu);
+						dispatcher.fire(new GameDataEvent(client, client.getData()));
+					}
+				}).start();
+			}
+		});
+		menu.add(editMenu);
 
-    editMenu = new JMenuItem("teach2");
-    editMenu.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        new Thread(new Runnable() {
+		// clear all $$$ comments 
+		editMenu = new JMenuItem("clear");
+		editMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Teacher.clear(container);
+				dispatcher.fire(new GameDataEvent(client, client.getData()));
+			}
+		});
+		menu.add(editMenu);
 
-          public void run() {
-            TeacherNew.foo(container, new ProgressBarUI(client));
+		return menu;
+	}
 
-            dispatcher.fire(new GameDataEvent(client, client.getData()));
-          }
-        }).start();
-      }
-    });
-    menu.add(editMenu);
+	/**
+	 * @see magellan.client.extern.MagellanPlugIn#quit(boolean)
+	 */
+	public void quit(boolean storeSettings) {
+		// do nothing
+	}
 
-    editMenu = new JMenuItem("clear");
-    editMenu.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        Teacher.clear(container);
-        dispatcher.fire(new GameDataEvent(client, client.getData()));
-      }
-    });
-    menu.add(editMenu);
-
-    return menu;
-  }
-
-  /**
-   * @see magellan.client.extern.MagellanPlugIn#quit(boolean)
-   */
-  public void quit(boolean storeSettings) {
-    // do nothing
-  }
-
-  public PreferencesFactory getPreferencesProvider() {
-    return null;
-  }
+	public PreferencesFactory getPreferencesProvider() {
+		return null;
+	}
 }
