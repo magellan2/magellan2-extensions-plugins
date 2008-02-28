51bc2e5b9c966fe09965472c69564931ffa6aee9
stm_ <stm_@1c5abfc5-b992-4a1f-8ccd-9ce068e22774>
2008-02-28 00:00:26 +0000

    improved genetic algorithm; unit context menu

--- Teacher/src/magellan/plugin/teacher/TeachPlugin.java
+++ Teacher/src/magellan/plugin/teacher/TeachPlugin.java
@@ -24,17 +24,21 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.MissingResourceException;
 import java.util.Properties;
 import java.util.ResourceBundle;
+import java.util.StringTokenizer;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
@@ -43,6 +47,7 @@ import magellan.client.event.EventDispatcher;
 import magellan.client.extern.MagellanPlugIn;
 import magellan.client.swing.ProgressBarUI;
 import magellan.client.swing.context.UnitContainerContextMenuProvider;
+import magellan.client.swing.context.UnitContextMenuProvider;
 import magellan.client.swing.preferences.PreferencesAdapter;
 import magellan.client.swing.preferences.PreferencesFactory;
 import magellan.library.GameData;
@@ -60,14 +65,14 @@ import magellan.library.utils.logging.Logger;
  * // $$L Talent1 value1 Talent2 value2
  * </pre>
  * 
- * denotes a student learning two skills of different values
+ * denotes a student learning two skills of different values.
  * 
  * <pre>
  * // $$L ALLES maxVal minVal
  * </pre>
  * 
  * means that the student will learn any talent it already knows. The highest skill will have
- * maxVal; skills with lower ranks will have values down to minVal
+ * maxVal; skills with lower ranks will have values down to minVal.
  * 
  * <pre>
  * // $$T Talent1 maxDiff1 Talent2 maxDiff2
@@ -75,36 +80,35 @@ import magellan.library.utils.logging.Logger;
  * 
  * denotes a teacher teaching two skills. Students having a skill differing more than maxDiff from
  * the teachers talent are penalized. maxDiff==0 has the special meaning that there is no such
- * penalty. maxDiff==1 means that the teacher will not teach this talent
+ * penalty. maxDiff==1 means that the teacher will not teach this talent.
  * 
  * <pre>
  * // $$T ALLES maxDiff
  * </pre>
  * 
- * denotes a teacher teaching all the skills he knows.
+ * denotes a teacher teaching all the skills he knows,
  * 
  * <pre>
  * // $$T ALLES 0 Hiebwaffen 2
  * </pre>
  * 
- * would also be feasible
+ * would also be feasible.
  * 
  * <pre>
  * // $namespace1$T ...
- * </pre>
- * <pre>
  * // $namespace1$L ...
  * </pre>
  * 
- * defines an order belonging to a namespace; it can be used to teaching only to units with certain
- * namespace
+ * defines an order belonging to a namespace; it can be used to teaching only to units having a
+ * certain namespace.
  * 
- * It is feasible (in fact, desirable) for a unit to be teacher and student at the same time.
+ * It is feasible for a unit to be teacher and student at the same time. In fact, any unit with
+ * teaching orders <i>must</i> also have a learning order.
  * 
  * @author stm
  */
 public class TeachPlugin implements MagellanPlugIn, UnitContainerContextMenuProvider,
-		ActionListener {
+		UnitContextMenuProvider, ActionListener {
 	private static Logger log = null;
 
 	private Client client = null;
@@ -117,7 +121,6 @@ public class TeachPlugin implements MagellanPlugIn, UnitContainerContextMenuProv
 	 * An enum for all action types in this plugin.
 	 * 
 	 * @author Thoralf Rickert
-	 * @version 1.0, 11.09.2007
 	 */
 	public enum PlugInAction {
 		EXECUTE("mainmenu.execute"), EXECUTE_ALL("mainmenu.executeall"), CLEAR("mainmenu.clear"), CLEAR_ALL(
@@ -155,7 +158,6 @@ public class TeachPlugin implements MagellanPlugIn, UnitContainerContextMenuProv
 
 		log = Logger.getInstance(TeachPlugin.class);
 		log.info(getName() + " initialized...");
-
 	}
 
 	/**
@@ -175,20 +177,20 @@ public class TeachPlugin implements MagellanPlugIn, UnitContainerContextMenuProv
 		JMenu menu = new JMenu(getString("plugin.teacher.mainmenu.title"));
 		items.add(menu);
 
-//		JMenuItem executeMenu = new JMenuItem(getString("mainmenu.execute.title"));
-//		executeMenu.setActionCommand(PlugInAction.EXECUTE.getID());
-//		executeMenu.addActionListener(this);
-//		menu.add(executeMenu);
-//
+		// JMenuItem executeMenu = new JMenuItem(getString("mainmenu.execute.title"));
+		// executeMenu.setActionCommand(PlugInAction.EXECUTE.getID());
+		// executeMenu.addActionListener(this);
+		// menu.add(executeMenu);
+		//
 		JMenuItem executeAllMenu = new JMenuItem(getString("plugin.teacher.mainmenu.executeall.title"));
 		executeAllMenu.setActionCommand(PlugInAction.EXECUTE_ALL.getID());
 		executeAllMenu.addActionListener(this);
 		menu.add(executeAllMenu);
 
-//		JMenuItem clearMenu = new JMenuItem(getString("mainmenu.clear.title"));
-//		clearMenu.setActionCommand(PlugInAction.CLEAR.getID());
-//		clearMenu.addActionListener(this);
-//		menu.add(clearMenu);
+		// JMenuItem clearMenu = new JMenuItem(getString("mainmenu.clear.title"));
+		// clearMenu.setActionCommand(PlugInAction.CLEAR.getID());
+		// clearMenu.addActionListener(this);
+		// menu.add(clearMenu);
 
 		JMenuItem clearAllMenu = new JMenuItem(getString("plugin.teacher.mainmenu.clearall.title"));
 		clearAllMenu.setActionCommand(PlugInAction.CLEAR_ALL.getID());
@@ -223,6 +225,15 @@ public class TeachPlugin implements MagellanPlugIn, UnitContainerContextMenuProv
 		menu.add(editMenu);
 
 		// clear all $$$ comments
+		editMenu = new JMenuItem(getString("plugin.teacher.contextmenu.parse.title"));
+		editMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				doParse(container.units());
+			}
+		});
+		menu.add(editMenu);
+
+		// clear all $$$ comments
 		editMenu = new JMenuItem(getString("plugin.teacher.contextmenu.clear.title"));
 		editMenu.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -232,6 +243,161 @@ public class TeachPlugin implements MagellanPlugIn, UnitContainerContextMenuProv
 		menu.add(editMenu);
 
 		return menu;
+	}
+
+	public JMenuItem createContextMenu(EventDispatcher dispatcher, GameData data, final Unit unit,
+			final Collection selectedObjects) {
+		JMenu menu = new JMenu(getString("plugin.teacher.contextmenu.title"));
+
+		JMenuItem addLearnMenu = new JMenuItem(getString("plugin.teacher.contextmenu.addlearn.title",
+				new Object[] { unit.getName(), unit.getID().toString() }));
+		addLearnMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String userInput = JOptionPane.showInputDialog(client,
+						getString("plugin.teacher.addlearn.askTalent.message"), "ALLES 100 51");
+				if (userInput != null) {
+					try {
+						StringTokenizer st = new StringTokenizer(userInput, " ", false);
+						String talent = st.nextToken();
+						double value = Double.parseDouble(st.nextToken());
+						Order newOrder;
+						if (talent.equals(Order.ALL)) {
+							double value2 = Double.parseDouble(st.nextToken());
+							newOrder = new Order(value, value2);
+						} else {
+							newOrder = new Order(talent, value);
+						}
+						Collection<Unit> units = null;
+						if (selectedObjects != null) {
+							units = new ArrayList<Unit>(selectedObjects.size());
+							for (Object o : selectedObjects) {
+								if (o instanceof Unit)
+									units.add((Unit) o);
+							}
+						}
+
+						addOrder(unit, units, newOrder);
+					} catch (Exception ex) {
+						log.warn(ex);
+						JOptionPane.showMessageDialog(client, getString("plugin.teacher.addlearn.error"));
+					}
+				}
+			}
+		});
+		menu.add(addLearnMenu);
+
+		JMenuItem addTeachMenu = new JMenuItem(getString("plugin.teacher.contextmenu.addteach.title",
+				new Object[] { unit.getName(), unit.getID().toString() }));
+		addTeachMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String userInput = JOptionPane.showInputDialog(client,
+						getString("plugin.teacher.addteach.askTalent.message"), "ALLES 0");
+				if (userInput != null) {
+					try {
+						StringTokenizer st = new StringTokenizer(userInput, " ", false);
+						String talent = st.nextToken();
+						int diff = Integer.parseInt(st.nextToken());
+						Order newOrder = new Order(talent, diff);
+						Collection<Unit> units = null;
+						if (selectedObjects != null) {
+							units = new ArrayList<Unit>(selectedObjects.size());
+							for (Object o : selectedObjects) {
+								if (o instanceof Unit)
+									units.add((Unit) o);
+							}
+						}
+
+						addOrder(unit, units, newOrder);
+					} catch (Exception ex) {
+						log.warn(ex);
+						JOptionPane.showMessageDialog(client, getString("plugin.teacher.addlearn.error"));
+					}
+				}
+			}
+		});
+		// executeMenu.setEnabled(commands.hasCommands(unit));
+		menu.add(addTeachMenu);
+
+		JMenuItem delLearnMenu = new JMenuItem(getString("plugin.teacher.contextmenu.dellearn.title",
+				new Object[] { unit.getName(), unit.getID().toString() }));
+		delLearnMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String talent = JOptionPane.showInputDialog(client,
+						getString("plugin.teacher.dellearn.askTalent.message"), "ALLES");
+				if (talent != null) {
+					try {
+						Order newOrder;
+						if (talent.equals(Order.ALL))
+							newOrder = new Order(1d, 1d);
+						else
+							newOrder = new Order(talent, 1d);
+						Collection<Unit> units = null;
+						if (selectedObjects != null) {
+							units = new ArrayList<Unit>(selectedObjects.size());
+							for (Object o : selectedObjects) {
+								if (o instanceof Unit)
+									units.add((Unit) o);
+							}
+						}
+
+						delOrder(unit, units, newOrder);
+					} catch (Exception ex) {
+						log.warn(ex);
+						JOptionPane.showMessageDialog(client, getString("plugin.teacher.addlearn.error"));
+					}
+				}
+			}
+		});
+		menu.add(delLearnMenu);
+
+		JMenuItem delTeachMenu = new JMenuItem(getString("plugin.teacher.contextmenu.delteach.title",
+				new Object[] { unit.getName(), unit.getID().toString() }));
+		delTeachMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String talent = JOptionPane.showInputDialog(client,
+						getString("plugin.teacher.delteach.askTalent.message"), "ALLES");
+				if (talent != null) {
+					try {
+						Order newOrder;
+						if (talent.equals(Order.ALL))
+							newOrder = new Order(0);
+						else
+							newOrder = new Order(talent, 0);
+						Collection<Unit> units = null;
+						if (selectedObjects != null) {
+							units = new ArrayList<Unit>(selectedObjects.size());
+							for (Object o : selectedObjects) {
+								if (o instanceof Unit)
+									units.add((Unit) o);
+							}
+						}
+
+						delOrder(unit, units, newOrder);
+					} catch (Exception ex) {
+						log.warn(ex);
+						JOptionPane.showMessageDialog(client, getString("plugin.teacher.addlearn.error"));
+					}
+				}
+			}
+
+		});
+		// executeMenu.setEnabled(commands.hasCommands(unit));
+		menu.add(delTeachMenu);
+
+		return menu;
+
+	}
+
+	protected void delOrder(Unit unit, Collection selectedObjects, Order newOrder) {
+		Teacher.delOrder(selectedObjects != null ? selectedObjects : Collections.singletonList(unit),
+				namespace, newOrder);
+		client.getDispatcher().fire(new GameDataEvent(client, client.getData()));
+	}
+
+	protected void addOrder(Unit unit, Collection<Unit> selectedObjects, Order newOrder) {
+		Teacher.addOrder(selectedObjects != null ? selectedObjects : Collections.singletonList(unit),
+				namespace, newOrder);
+		client.getDispatcher().fire(new GameDataEvent(client, client.getData()));
 	}
 
 	public void actionPerformed(ActionEvent e) {
@@ -268,6 +434,17 @@ public class TeachPlugin implements MagellanPlugIn, UnitContainerContextMenuProv
 
 	}
 
+	private void doParse(final Collection<Unit> values) {
+		new Thread(new Runnable() {
+
+			public void run() {
+				Teacher.parse(values, namespace, new ProgressBarUI(client));
+				client.getDispatcher().fire(new GameDataEvent(client, client.getData()));
+			}
+		}).start();
+
+	}
+
 	/**
 	 * @see magellan.client.extern.MagellanPlugIn#quit(boolean)
 	 */
@@ -298,14 +475,13 @@ public class TeachPlugin implements MagellanPlugIn, UnitContainerContextMenuProv
 			panel.setBorder(new javax.swing.border.TitledBorder(BorderFactory.createEtchedBorder(),
 					getString("plugin.teacher.preferences.label.namespace")));
 
-			GridBagConstraints con = new GridBagConstraints(0, 0, 1, 1, 0, 0,
-					GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(3, 3, 3, 3),
-					0, 0);
+			GridBagConstraints con = new GridBagConstraints(0, 0, 1, 1, 0, 0, GridBagConstraints.WEST,
+					GridBagConstraints.HORIZONTAL, new Insets(3, 3, 3, 3), 0, 0);
 
 			JLabel lblNamespace = new JLabel(getString("plugin.teacher.preferences.label.namespace"));
 			panel.add(lblNamespace, con);
 
-			txtNamespace = new JTextArea();
+			txtNamespace = new JTextArea(namespace);
 			txtNamespace.setMinimumSize(new Dimension(100, 20));
 			txtNamespace.setPreferredSize(new java.awt.Dimension(100, 20));
 			// txtNamespace.setText("stm");
@@ -327,18 +503,27 @@ public class TeachPlugin implements MagellanPlugIn, UnitContainerContextMenuProv
 
 	}
 
-	  private static final String BUNDLE_NAME = "magellan.plugin.teacher.teacher_resources";
+	private static final String BUNDLE_NAME = "magellan.plugin.teacher.teacher_resources";
 
-	  private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
-	  public static String getString(String key) {
+	protected static String getString(String key) {
 		ResourceBundle bundle = ResourceBundle.getBundle(BUNDLE_NAME, Locales.getGUILocale());
-	    try {
-	      return bundle.getString(key);
-	    } catch (MissingResourceException e) {
-	      log.warn("resource key "+key+" not found in bundle "+BUNDLE_NAME+", "+Locales.getGUILocale());
-	      return '!' + key + '!';
-	    }
-	  }
+		try {
+			return bundle.getString(key);
+		} catch (MissingResourceException e) {
+			log.warn("resource key " + key + " not found in bundle " + BUNDLE_NAME + ", "
+					+ Locales.getGUILocale());
+			return '!' + key + '!';
+		}
+	}
+
+	protected static String getString(String key, Object[] args) {
+		String value = getString(key);
+		if (value != null) {
+			value = new MessageFormat(value).format(args);
+		}
+		return value;
+	}
 
 }
