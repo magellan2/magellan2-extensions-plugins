a00251c64fed0aacbe6f22f3969bc0de09c17890
stm2 <stm2@1c5abfc5-b992-4a1f-8ccd-9ce068e22774>
2013-07-05 12:56:35 +0000

    TeachPlugin: new published version 0.15 for nightly

--- trunk/magellan-ext-plugins/Teacher/src/magellan/plugin/teacher/TeachPlugin.java
+++ trunk/magellan-ext-plugins/Teacher/src/magellan/plugin/teacher/TeachPlugin.java
@@ -110,7 +110,7 @@ import magellan.library.utils.logging.Logger;
  * teaching orders <i>must</i> also have a learning order.
  * 
  * @author stm
- * @version 0.14
+ * @version 0.15
  */
 public class TeachPlugin implements MagellanPlugIn, UnitContainerContextMenuProvider,
 		UnitContextMenuProvider, ActionListener {
@@ -159,7 +159,7 @@ public class TeachPlugin implements MagellanPlugIn, UnitContainerContextMenuProv
 
 	protected Boolean running = false;
 
-	public static final String version = "0.14";
+	public static final String version = "0.15";
 
 	public TeachPlugin() {
 		namespaces = new LinkedList<String>();
